79945a9ce601532e074f282cb35cb5ed8eb9270f
Abel Salgado Romero <abelromero@gmail.com>
2022-05-08 10:08:01 +0200

    Initial Java setup
    * Build app: ./gradlew installDist
    * Run app:   java -cp "app/build/install/app/lib/*" org.abelsromero.nativ.App

---
+++ app/src/main/java/org/abelsromero/nativ/App.java
@@ -0,0 +1,10 @@
+package org.abelsromero.nativ;
+
+import org.abelsromero.utilities.StringUtils;
+
+public class App {
+    
+    public static void main(String[] args) {
+        System.out.println(StringUtils.toUpperCase("Hello world!!"));
+    }
+}
